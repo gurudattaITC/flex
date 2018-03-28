126d4e0f50c894c46f015376ce2d2192748f6554
Shrinivasgurunath A <28999@ITCINFOTECH.COM>
2018-03-28 16:23:36 +0530

    More Test files

---
+++ application/tests/com/cust/product/TestAddProduct.java
@@ -0,0 +1,10 @@
+package com.cust.product;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+public class TestAddProduct {
+   @Test
+   public void testString() {
+      String str= "Junit, Hello world!" ;
+      assertEquals("Junit, Hello world!",str);
+   }
+}
