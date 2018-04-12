fc80d22a90dd39e63db3270d56105fbda31a9655
sriniprao <37864053+sriniprao@users.noreply.github.com>
2018-04-12 18:10:47 +0530

    Update CreateProduct.java

--- application/scripts/custom_scripts/src/com/stc/product/CreateProduct.java
+++ application/scripts/custom_scripts/src/com/stc/product/CreateProduct.java
@@ -89,7 +89,7 @@ public class CreateProduct extends TestManager {
 			productSaveButton = driver.findElement(By.id("saveButton"));
 			productSaveButton.click();
 			
-			drive.quit();
+			driver.quit();
 		
 		} catch(Exception e) {
 			System.out.println(e);
