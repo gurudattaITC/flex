e3843704cc5f6918b404a6adec07ab214068eb87
gurudattaITC <gurudatta.karmi@itcinfotech.com>
2018-05-29 20:55:47 +0530

    Update UpdateAccessRemovedOnSKU.java
    testing by throwing exception.

--- application/scripts/custom_scripts/src/com/stc/product/UpdateAccessRemovedOnSKU.java
+++ application/scripts/custom_scripts/src/com/stc/product/UpdateAccessRemovedOnSKU.java
@@ -80,13 +80,13 @@ public class UpdateAccessRemovedOnSKU extends TestManager {
 				assertTrue(false, "Update Colorway Access is not Removed.");
 				System.out.println("I am trying now with assertEquals...");
 				assertEquals((2>3), (3>2), "Update Colorway Access is not Removed.");
-				//throw new Exception("Update Colorway Access is not Removed.");
+				throw new Exception("Update Colorway Access is not Removed.");
 			}
 		System.out.println("Everyone cheated me...I am trying one last option..pls help me");
 		assertTrue((2>3), "Update Jenkins Job.");
 		} catch(Exception e) {
 			System.out.println(e);
-			//throw e;
+			throw e;
 		}
 		
 		//driver.quit();
